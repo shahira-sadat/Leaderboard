e62522362776b0af59b9c245a1585e9cd15ff735
shahira-sadat <shahira.sadat1@gmail.com>
2022-07-18 18:16:40 +0430

    Add codes for adding and generating scores in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
 import data from './modules/data.js';
 import generateScores from './modules/score.js';
+
+
+// Generate scores
+generateScores(data);
+
+// Add a score
+const container = document.querySelector('.form');
+const inputName = document.querySelector('#input-name');
+const inputScore = document.querySelector('#input-score');
+
+container.onsubmit = (e) => {
+  e.preventDefault();
+
+  const name = inputName.value;
+  const score = inputScore.value;
+  data.push({ name, score });
+  generateScores(data);
+}; 
